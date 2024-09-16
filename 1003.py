d6fa91c7c1305b536c3840619654fc14e067cbf4
RICH999999999 <geo-rich@mail.ru>
2024-09-16 17:20:07 +0300

    1003.py

--- 1003.py
+++ 1003.py
@@ -15,7 +15,7 @@ def get_matrix(n, m, value):
     return matrix
 #Пример использования функции
 result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 13)
+result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 print(result1)
 print(result2)
